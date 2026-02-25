eb168f85e6a59e0b2838df40dded20660432df11
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2026-02-25 09:22:15 +0500

    chore: инициализация проетка
    --ALLAPP: инициализирован проект
    --pretterrc | stylelintrc: установлены линтеры

---
+++ src/App.tsx
@@ -0,0 +1,4 @@
+// Комппонент приложения
+export const App = () => {
+    return <div>Hello world</div>
+}
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+// Импорты
+import { createRoot } from 'react-dom/client'
+import { App } from './App'
+
+// Рендер приложения
+const rootElemet = document.getElementById('root') as HTMLElement
+const reactRoot = createRoot(rootElemet)
+
+reactRoot.render(<App />)
